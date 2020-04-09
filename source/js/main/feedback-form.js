30ab73a5b32f3aec1e330be6950add15a5a8ab84
Marina <marishkina@gmail.com>
2020-04-09 13:41:11 +0800

    правки по ревью наставника и баглист от тест без резины

--- source/js/main/feedback-form.js
+++ source/js/main/feedback-form.js
@@ -3,40 +3,42 @@
 (function () {
 
   var form = document.querySelector('.feedback__form');
-  var clientName = form.querySelector('[name=name]');
-  var clientPhoneNumber = form.querySelector('[name=phone]');
-  var formQuestion = form.querySelector('[name=user-question]');
-  var storage = localStorage.getItem('clientName');
-  storage = localStorage.getItem('clientNumber');
-  storage = localStorage.getItem('formQuestion');
 
-  var isStorageSupport = true;
-  storage = '';
-
-  try {
-    storage = localStorage.getItem('clentName');
-    storage = localStorage.getItem('clientPhoneNumber');
+  if (form) {
+    var clientName = form.querySelector('[name=name]');
+    var clientPhoneNumber = form.querySelector('[name=tel]');
+    var formQuestion = form.querySelector('[name=your-question]');
+    var storage = localStorage.getItem('clientName');
+    storage = localStorage.getItem('clientNumber');
     storage = localStorage.getItem('formQuestion');
-  } catch (err) {
-    isStorageSupport = false;
-  }
 
-  if (storage) {
-    clientName.value = storage;
-    clientPhoneNumber.focus();
-  } else {
-    clientName.focus();
-  }
+    var isStorageSupport = true;
+    storage = '';
 
-  form.addEventListener('submit', function (evt) {
-    if (!clientName.value || !clientPhoneNumber.value || !formQuestion.value) {
-      evt.preventDefault();
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem('clientName', clientName.value);
-        localStorage.setItem('clientPhoneNumber', clientPhoneNumber.value);
-        localStorage.setItem('formQuestion', formQuestion.value);
-      }
+    try {
+      storage = localStorage.getItem('clientName');
+      storage = localStorage.getItem('clientNumber');
+      storage = localStorage.getItem('formQuestion');
+    } catch (err) {
+      isStorageSupport = false;
     }
-  });
+
+    if (storage) {
+      clientName.value = storage;
+      clientPhoneNumber.value = storage;
+      formQuestion.value = storage;
+    }
+
+    form.addEventListener('submit', function (evt) {
+      if (!clientName.value || !clientPhoneNumber.value || !formQuestion.value) {
+        evt.preventDefault();
+      } else {
+        if (isStorageSupport) {
+          localStorage.setItem('clientName', clientName.value);
+          localStorage.setItem('clientNumber', clientPhoneNumber.value);
+          localStorage.setItem('formQuestion', formQuestion.value);
+        }
+      }
+    });
+  }
 })();
--- source/js/main/modal.js
+++ source/js/main/modal.js
@@ -3,69 +3,78 @@
 (function () {
   var link = document.querySelector('.page-header__call-button');
   var popup = document.querySelector('.modal');
-  var close = popup.querySelector('.modal__close-button');
-  var userName = popup.querySelector('[name=user-name]');
-  var phoneNumber = popup.querySelector('[name=phone-number]');
-  var question = popup.querySelector('[name=user-question]');
-  var storage = localStorage.getItem('userName');
-  storage = localStorage.getItem('phoneNumber');
-  storage = localStorage.getItem('question');
 
-  var isStorageSupport = true;
-  storage = '';
+  if (popup) {
 
-  try {
-    storage = localStorage.getItem('userName');
+    var close = popup.querySelector('.modal__close-button');
+    var userName = popup.querySelector('[name=user-name]');
+    var phoneNumber = popup.querySelector('[name=phone-number]');
+    var question = popup.querySelector('[name=user-question]');
+    var storage = localStorage.getItem('userName');
     storage = localStorage.getItem('phoneNumber');
     storage = localStorage.getItem('question');
-  } catch (err) {
-    isStorageSupport = false;
+
+    var isStorageSupport = true;
+    storage = '';
+
+    try {
+      storage = localStorage.getItem('userName');
+      storage = localStorage.getItem('phoneNumber');
+      storage = localStorage.getItem('question');
+    } catch (err) {
+      isStorageSupport = false;
+    }
+
+    link.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      popup.classList.add('modal-show');
+      document.body.style.overflow = 'hidden';
+
+      if (storage) {
+        userName.value = storage;
+        phoneNumber.focus();
+      } else {
+        userName.focus();
+      }
+    });
+
+    close.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      popup.classList.remove('modal-show');
+      document.body.style.overflow = 'visible';
+    });
+
+    popup.addEventListener('submit', function (evt) {
+      if (!userName.value || !phoneNumber.value || !question.value) {
+        evt.preventDefault();
+      } else {
+        if (isStorageSupport) {
+          localStorage.setItem('userName', userName.value);
+          localStorage.setItem('phoneNumber', phoneNumber.value);
+          localStorage.setItem('question', question.value);
+          document.body.style.overflow = 'visible';
+        }
+      }
+    });
+
+    window.addEventListener('keydown', function (evt) {
+      if (evt.code === 'Escape') {
+        evt.preventDefault();
+        if (popup.classList.contains('modal-show')) {
+          popup.classList.remove('modal-show');
+          document.body.style.overflow = 'visible';
+        }
+      }
+    });
+
+    window.addEventListener('click', function (evt) {
+      if (evt.target === popup) {
+        evt.preventDefault();
+        if (popup.classList.contains('modal-show')) {
+          popup.classList.remove('modal-show');
+          document.body.style.overflow = 'visible';
+        }
+      }
+    });
   }
-
-  link.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popup.classList.add('modal-show');
-
-    if (storage) {
-      userName.value = storage;
-      phoneNumber.focus();
-    } else {
-      userName.focus();
-    }
-  });
-
-  close.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popup.classList.remove('modal-show');
-  });
-
-  popup.addEventListener('submit', function (evt) {
-    if (!userName.value || !phoneNumber.value || !question.value) {
-      evt.preventDefault();
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem('userName', userName.value);
-        localStorage.setItem('phoneNumber', phoneNumber.value);
-        localStorage.setItem('question', question.value);
-      }
-    }
-  });
-
-  window.addEventListener('keydown', function (evt) {
-    if (evt.code === 'Escape') {
-      evt.preventDefault();
-      if (popup.classList.contains('modal-show')) {
-        popup.classList.remove('modal-show');
-      }
-    }
-  });
-
-  window.addEventListener('click', function (evt) {
-    if (evt.target === popup) {
-      evt.preventDefault();
-      if (popup.classList.contains('modal-show')) {
-        popup.classList.remove('modal-show');
-      }
-    }
-  });
 })();
