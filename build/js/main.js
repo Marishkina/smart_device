80912b2b1e5d6c695cb650e22efe607e16f8b89a
Marina <marishkina@gmail.com>
2020-04-16 12:18:47 +0800

    правит видимость  навигации в футере на планшете и десктопе

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,15 @@
 'use strict';
 
 (function () {
+  $(window).resize(function () {
+    var width = $('body').innerWidth();
+    if (width > 767) {
+      $('.sub__list').css('display', 'flex');
+    } else {
+      $('.sub__list').css('display', 'none');
+    }
+  });
+
   $(document).ready(function () {
     $('.footer__accordion > .footer__accordion-list > .sub > .footer__accordion-link').click(function () {
       $(this).toggleClass('active').next().slideToggle();
